cc53b6f2727d595087b9207a2a840b747a6ad2d5
Christopher Neely <christopher.neely1200@gmail.com>
2020-12-30 17:02:20 -0800

    Final comments and small modularization

--- type_checking.py
+++ type_checking.py
@@ -27,29 +27,35 @@ class TypeChecker:
             TypeChecker._clear_if_surpassed_max_size()
             # Get passed args as dict
             passed_args = inspect.signature(func).bind(*args, **kwargs).arguments
-            # Get allowed types of f
+            # Get types specified by type annotations
             specified_types = get_type_hints(func)
+            # Calculate id of function data
             cache_add = id([*args, *kwargs.values(), func.__name__])
+            # Check if cached
             if cache_add not in TypeChecker._cache:
+                # Check arguments passed to ensure valid
                 for arg_name, arg_type in specified_types.items():
                     if arg_name not in passed_args.keys():
                         continue
                     TypeChecker._check_type(arg_type, arg_name, passed_args)
+                # Add successful call to cache
                 TypeChecker._cache.add(cache_add)
+            # Get function output
             output = func(*args, **kwargs)
+            # Confirm output is valid
             if "return" in specified_types.keys():
-                arg_type = specified_types["return"]
-                if getattr(arg_type, "__args__", None) is not None:
-                    if not TypeChecker._check_union(arg_type, output):
-                        raise TypeError("return type does not match {}".format(" or ".join(list(map(str, arg_type)))))
-                    # Type is a non-Union
-                else:
-                    if not isinstance(output, arg_type):
-                        raise TypeError("return type does not match {}".format(" or ".join(list(map(str, arg_type)))))
-
+                TypeChecker._validate_output(specified_types, output)
             return output
 
         return fxn
+
+    @staticmethod
+    def _clear_if_surpassed_max_size():
+        """ Check if cache size surpasses largest allowed and clear
+
+        """
+        if TypeChecker.current_cache_size() >= TypeChecker._max_cache_size:
+            TypeChecker.clear_cache()
 
     @staticmethod
     def _check_union(arg_type: Union, passed_value: object) -> bool:
@@ -63,6 +69,21 @@ class TypeChecker:
             if isinstance(passed_value, avail_arg_type):
                 return True
         return False
+
+    @staticmethod
+    def _validate_output(specified_types, output):
+        """ Confirm that output generate by function call has expected type. Raises TypeError if issue
+
+        :param specified_types: Dictionary of annotated types
+        :param output: Function call output
+        """
+        arg_type = specified_types["return"]
+        if getattr(arg_type, "__args__", None) is not None:
+            if not TypeChecker._check_union(arg_type, output):
+                raise TypeError("return type does not match {}".format(" or ".join(list(map(str, arg_type)))))
+        else:
+            if not isinstance(output, arg_type):
+                raise TypeError("return type does not match {}".format(" or ".join(list(map(str, arg_type)))))
 
     @staticmethod
     def _check_type(arg_type, arg_name: str, passed_args: Dict):
@@ -108,11 +129,3 @@ class TypeChecker:
             TypeChecker._max_cache_size = max_size
             return
         raise TypeError("Must provide positive cache size")
-
-    @staticmethod
-    def _clear_if_surpassed_max_size():
-        """ Check if cache size surpasses largest allowed and clear
-
-        """
-        if TypeChecker.current_cache_size() >= TypeChecker._max_cache_size:
-            TypeChecker.clear_cache()
