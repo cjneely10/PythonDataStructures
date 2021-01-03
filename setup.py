0fde29098a2a9b11798206ff38c8f194b10d2b1c
Christopher Neely <christopher.neely1200@gmail.com>
2021-01-02 17:40:06 -0800

    Remove added indentation

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-      name='PythonDataStructures',
-      version='0.1',
-      description='Simple data structures and decorators for use in code development',
-      url='https://github.com/cjneely10/PythonDataStructures',
-      author='Christopher Neely',
-      author_email='christopher.neely1200@gmail.com',
-      license='GPL-3.0',
-      packages=['data_structures'],
-      zip_safe=False
+    name='PythonDataStructures',
+    version='0.1',
+    description='Simple data structures and decorators for use in code development',
+    url='https://github.com/cjneely10/PythonDataStructures',
+    author='Christopher Neely',
+    author_email='christopher.neely1200@gmail.com',
+    license='GPL-3.0',
+    packages=['data_structures'],
+    zip_safe=False
 )
